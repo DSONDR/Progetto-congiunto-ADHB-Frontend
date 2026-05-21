76fec7cc2269bef0f06739a1b85967a0629b0ec4
Andrea Dosi <andre04.dosi@gmail.com>
2026-05-21 10:46:22 +0200

    Stile di login e registrazione

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 // Ipotetico componente di login (lo creeremo in futuro)
-// import { LoginComponent } from './features/auth/login.component'; 
+import { LoginComponent } from './component/login/login.component'; 
+import { RegistrazioneComponent } from './component/registrazione/registrazione.component'; 
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  //Percorsi reali di routing
+  { path: 'login', component: LoginComponent },
+  { path: 'registrazione', component: RegistrazioneComponent },
   // Prepariamo i "buchi" per i futuri link del menu
   { path: 'eventi', component: HomeComponent },
   { path: 'calendario', component: HomeComponent },
   { path: 'abbonamenti', component: HomeComponent },
   { path: 'assistenza', component: HomeComponent },
-  { path: 'login', component: HomeComponent }, // Placeholder temporaneo: qui andrà la pagina di login vera
   { path: 'area-personale', component: HomeComponent }
 ];
---
+++ src/app/component/registrazione/registrazione.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-registrazione',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './registrazione.component.html',
+  styleUrl: './registrazione.component.scss'
+})
+export class RegistrazioneComponent {}
