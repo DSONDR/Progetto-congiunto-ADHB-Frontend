b3ed30624ec7abd72c295e2d628213fb3948d26c
ahmedinho04 <ahmedbini0804@gmail.com>
2026-05-27 23:26:33 +0200

    Update usa-abbonamento.service.ts

--- src/app/service/usa-abbonamento.service.ts
+++ src/app/service/usa-abbonamento.service.ts
@@ -1,6 +1,8 @@
+// Import Angular e RxJS
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+// DTO richiesta
 import { UsaAbbonamentoRequest } from '../dto/request/iscrizioni-request.dto';
 
 /**
@@ -17,26 +19,32 @@ import { UsaAbbonamentoRequest } from '../dto/request/iscrizioni-request.dto';
   providedIn: 'root'
 })
 export class UsaAbbonamentoService {
+  // Endpoint API principale
   private apiUrl = '/api/usa-abb';
 
   constructor(private http: HttpClient) {}
 
+  // Recupera tutti i record
   getAll(): Observable<UsaAbbonamentoRequest[]> {
     return this.http.get<UsaAbbonamentoRequest[]>(this.apiUrl);
   }
 
+  // Recupera record tramite ID
   getById(id: number): Observable<UsaAbbonamentoRequest> {
     return this.http.get<UsaAbbonamentoRequest>(`${this.apiUrl}/${id}`);
   }
 
+  // Creazione nuovo utilizzo abbonamento
   create(usaAbbonamento: UsaAbbonamentoRequest): Observable<UsaAbbonamentoRequest> {
     return this.http.post<UsaAbbonamentoRequest>(this.apiUrl, usaAbbonamento);
   }
 
+  // Aggiornamento record
   update(id: number, usaAbbonamento: UsaAbbonamentoRequest): Observable<UsaAbbonamentoRequest> {
     return this.http.put<UsaAbbonamentoRequest>(`${this.apiUrl}/${id}`, usaAbbonamento);
   }
 
+  // Eliminazione record
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
