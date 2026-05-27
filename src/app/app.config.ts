80c76daf663bd45e809300c125e291f8de92fdc6
ahmedinho04 <ahmedbini0804@gmail.com>
2026-05-27 23:27:34 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
+// Import configurazioni Angular
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+// Import routes applicazione
 import { routes } from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
 
+// Configurazione principale applicazione
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
